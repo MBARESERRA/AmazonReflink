55016614611576b9920843c7fdbbd5fa94639c79
MBARESERRA <62523599+MBARESERRA@users.noreply.github.com>
2020-11-19 16:09:56 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -4,4 +4,4 @@ $bitlytoken = 'R_c7d78316d223d5a1d7827d58d80e76be'; //inserisci qui dentro il to
 $api = '1169359396:AAE2uYgsJXm7blM5LMgbnq1hl4QLKsrxvR4'; //inserisci l'api token del tuo bot telegram [CAMBPO OBBLIGATORIO]
 $chats = []; //inserisci qui dentro i chatID che possono usare il bot se vuoi.
 $ref = 'nerdaiolipro-21'; //inserisci qui dentro il tuo tag amazon.
-$amzn = false; //Metti true per referrare anche i link amzn.to (sconsigliato). Consigliato lasciare false.
+$amzn = true; //Metti true per referrare anche i link amzn.to (sconsigliato). Consigliato lasciare false.
